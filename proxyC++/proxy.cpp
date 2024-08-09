cd7a3794ce117e786852abfd7307de7c8f271611
Adithya11811 <adithya11811@gmail.com>
2024-08-09 09:48:32 +0530

    feat: caching along with opens other websites

--- proxyC++/proxy.cpp
+++ proxyC++/proxy.cpp
@@ -41,7 +41,7 @@ char* convert_Request_to_string(struct ParsedRequest *req)
 
 
 	int request_size = strlen(req->method) + strlen(req->path) + strlen(req->version) + iHeadersLen + 4;
-	
+
 	char *serverReq;
 
 	serverReq = (char *) malloc(request_size + 1);
@@ -103,7 +103,7 @@ void writeToserverSocket(const char* buff_to_server,int sockfd,int buff_length)
 	string temp;
 
 	temp.append(buff_to_server);
-	
+
 	int totalsent = 0;
 
 	int senteach;
@@ -115,7 +115,7 @@ void writeToserverSocket(const char* buff_to_server,int sockfd,int buff_length)
 		}
 		totalsent += senteach;
 
-	}	
+	}
 
 }
 
@@ -137,7 +137,7 @@ void writeToclientSocket(const char* buff_to_server,int sockfd,int buff_length)
 		}
 		totalsent += senteach;
 
-	}	
+	}
 
 }
 
@@ -150,9 +150,9 @@ void writeToClient (int Clientfd, int Serverfd) {
 	char buf[MAX_BUF_SIZE];
 
 	while ((iRecv = recv(Serverfd, buf, MAX_BUF_SIZE, 0)) > 0) {
-	      writeToclientSocket(buf, Clientfd,iRecv);         // writing to client	    
-		memset(buf,0,sizeof buf);	
-	}      
+	      writeToclientSocket(buf, Clientfd,iRecv);         // writing to client
+		memset(buf,0,sizeof buf);
+	}
 
 
 	/* Error handling */
@@ -165,7 +165,7 @@ void writeToClient (int Clientfd, int Serverfd) {
 
 void* datafromclient(void* sockid)
 {
-	
+
 	int MAX_BUFFER_SIZE = 5000;
 
 	char buf[MAX_BUFFER_SIZE];
@@ -174,34 +174,30 @@ void* datafromclient(void* sockid)
 
 	char *request_message;  // Get message from URL
 
-	request_message = (char *) malloc(MAX_BUFFER_SIZE); 
+	request_message = (char *) malloc(MAX_BUFFER_SIZE);
 
 	if (request_message == NULL) {
 		fprintf (stderr," Error in memory allocation ! \n");
 		exit (1);
-	}	
+	}
 
 	request_message[0] = '\0';
 
 	int total_recieved_bits = 0;
 
 	while (strstr(request_message, "\r\n\r\n") == NULL) {  // determines end of request
-
 	  int recvd = recv(newsockfd, buf, MAX_BUFFER_SIZE, 0) ;
 
 	  if(recvd < 0 ){
 	  	fprintf (stderr," Error while recieving ! \n");
 		exit (1);
-	  				
 	  }else if(recvd == 0) {
 	  		break;
 	  } else {
-
 	  	total_recieved_bits += recvd;
-
 	  	/* if total message size greater than our string size,double the string size */
-
 	  	buf[recvd] = '\0';
+
 	  	if (total_recieved_bits > MAX_BUFFER_SIZE) {
 			MAX_BUFFER_SIZE *= 2;
 			request_message = (char *) realloc(request_message, MAX_BUFFER_SIZE);
@@ -222,28 +218,28 @@ void* datafromclient(void* sockid)
 
 	req = ParsedRequest_create();
 
-	if (ParsedRequest_parse(req, request_message, strlen(request_message)) < 0) {		
+	if (ParsedRequest_parse(req, request_message, strlen(request_message)) < 0) {
 		fprintf (stderr,"Error in request message..only http and get with headers are allowed ! \n");
 		exit(0);
 	}
 
-	if (req->port == NULL)             // if port is not mentioned in URL, we take default as 80 
+	if (req->port == NULL)             // if port is not mentioned in URL, we take default as 80
 		 req->port = (char *) "80";
-	
+
 
 	/*final request to be sent*/
-	
-	char*  browser_req  = convert_Request_to_string(req);		
-		
+
+	char*  browser_req  = convert_Request_to_string(req);
+
 	int iServerfd;
 
 	iServerfd = createserverSocket(req->host, req->port);
-	
+
 	/* To run on IIIT proxy, comment above line and uncomment below line */
-			
+
 	//char iiitproxy[] = "172.31.1.4";
 	//char iiitport[] = "8080";
-	
+
 	// iServerfd = createserverSocket(iiitproxy, iiitport);
 
 	writeToserverSocket(browser_req, iServerfd, total_recieved_bits);
@@ -251,7 +247,7 @@ void* datafromclient(void* sockid)
 
 
 	ParsedRequest_destroy(req);
-		
+
 	close(newsockfd);   // close the sockets
 	close(iServerfd);
 
@@ -262,16 +258,16 @@ void* datafromclient(void* sockid)
 
 }
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
 
 	int sockfd,newsockfd;
 
-	struct sockaddr_in serv_addr; 
+	struct sockaddr_in serv_addr;
 	struct sockaddr cli_addr;
 
 
-	if (argc<2) 
+	if (argc<2)
   	{
   		fprintf (stderr,"SORRY! Provide A Port ! \n");
   		return 1;
@@ -287,10 +283,10 @@ int main (int argc, char *argv[])
 
   	memset(&serv_addr,0,sizeof serv_addr);
 
-  	int  portno = atoi(argv[1]);           // argument through terminal 
- 	serv_addr.sin_family = AF_INET;     // ip4 family 
-  	serv_addr.sin_addr.s_addr = INADDR_ANY;  // represents for localhost i.e 127.0.0.1 
- 	serv_addr.sin_port = htons(portno); 
+  	int  portno = atoi(argv[1]);           // argument through terminal
+ 	serv_addr.sin_family = AF_INET;     // ip4 family
+  	serv_addr.sin_addr.s_addr = INADDR_ANY;  // represents for localhost i.e 127.0.0.1
+ 	serv_addr.sin_port = htons(portno);
 
 
  	int binded = bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
@@ -299,17 +295,18 @@ int main (int argc, char *argv[])
  		fprintf (stderr,"Error on binding! \n");
   		return 1;
  	}
-  	
+
   	listen(sockfd, 100);  // can have maximum of 100 browser requests
 
   	int clilen = sizeof(struct sockaddr);
 
+    fprintf(stdout,"Proxy Server Running on port: %d \n", portno);
 
   	while(1) {
-  		
+
   		/* A browser request starts here */
 
-  		newsockfd = accept(sockfd,&cli_addr, (socklen_t*) &clilen); 
+  		newsockfd = accept(sockfd,&cli_addr, (socklen_t*) &clilen);
 
   		if (newsockfd <0){
   			fprintf(stderr, "ERROR! On Accepting Request ! i.e requests limit crossed \n");
